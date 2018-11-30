56f8f27a10c774a072fbf507425a74cc6392354a
Rachel J <racheltyjohnson@gmail.com>
2018-12-01 09:08:46 +1100

    :rainbow: Add Quotes, get, and  print functios

--- js/script.js
+++ js/script.js
@@ -1,55 +1,84 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 1 - A Random Quote Generator
-******************************************/
+//array of quotes
+let quotes = [
+  {
+    quote: "Toto, I've got a feeling we're not in Kansas anymore.",
+    source: "Dorothy",
+    citation: "The Wizard of Oz",
+    year: 1939
+  },
+  {
+    quote: "May the Force be with you.",
+    source: "Yoda",
+    citation: "Star Wars",
+    year: 1977
+  },
+  {
+    quote: "E.T. phone home.",
+    source: "E.T.",
+    citation: "E.T. The Extra-Terrestrial",
+    year: 1982
+  },
+  {
+    quote: "Elementary, my dear Watson.",
+    source: "Sherlock Holmes",
+    citation: "The Adventures of Sherlock Holmes",
+    year: 1929
+  },
+  {
+    quote: "I'm king of the world!",
+    source: "Jack",
+    citation: "Titanic",
+    year: 1997
+  },
+]
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
-***/
-
+//getRandomQuote function
+function getRandomQuote(){
+  let randNum = Math.floor (Math.random() * quotes.length );
+  return quotes[randNum];
+}
 
 
 
 /***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
+  Create the `printQuote` function to:
    - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
+   - use the properties of the quote object stored in the variable to
      create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
+   - use conditionals to make sure the optional properties exist before
      they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
+   - set the `innerHTML` of the `quote-box` div to the HTML string.
 ***/
 
+function printQuote(){
+  //get a random quote
+  let theQuote = getRandomQuote();
+
+  //change the quote
+  let quoteContainer = document.querySelector('.quote');
+  quoteContainer.textContent = theQuote['quote'];
+
+  //change the source
+  let sourceContainer = document.querySelector('.source');
+  let sourceString = `${theQuote['source']}`;
+  if (theQuote['citation']!=''){
+    sourceString += `<span class="citation">${theQuote['citation']}</span>`
+  }
+  if (theQuote['citation']!=''){
+    sourceString += `<span class="citation">${theQuote['citation']}</span>`
+  }
+
+  sourceContainer.innerHTML = sourceString;
+}
 
 
 
 /***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
+  When the "Show another quote" button is clicked, the event listener
+  below will be triggered, and it will call, or "invoke", the `printQuote`
+  function. So do not make any changes to the line of code below this
   comment.
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
